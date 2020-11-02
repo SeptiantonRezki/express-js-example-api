b35a18ef6cc73f92bf90fce7f9bcef22c75cb09a
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-03 06:29:37 +0700

    Create Auth Router

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
+const authRouter = require('./auth');
+
 module.exports = (app) => {
+    app.use('/auth', authRouter);
+
     app.get(('/'), (req, res, next) => {
         res.send('Welcome to the hompage');
-    });
-    
-    
-    app.get('/user/:id/:postId', (req, res, next) => {
-        // localhost:5000/user/1/10?comment=first&like=first
-        console.log(req.params); // {id : '1', postId:'10'}
-        console.log(req.query);// {comment : 'first', like:'first'}
-        const host  = req.get('host');
-        console.log(host);
-        res.send('Welcome to user page');
     });
 };
