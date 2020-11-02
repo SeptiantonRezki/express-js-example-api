6aa265dcbd4df45431f67d4a6199027296793188
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-02 14:06:11 +0700

    Route Handling

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 
+
+app.get(('/'),(req, res, next) => {
+    // res.send('Welcome to the hompage');
+    // res.status(200).send('Welcome to the hompage'); => statis 200 succes 400 error 500 internal server error
+    res.json({
+        message : "welcome to homepe"
+    });
+    res.redirect('/user');
+});
+app.get('/user', (req, res, next) => {
+    res.send('Welcome to user page');
+})
+
+
 module.exports = app;
