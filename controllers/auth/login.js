594924fb4626c101432e3e00582fe8c926ac7921
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-03 06:41:47 +0700

    introduction MVC and use controllers

---
+++ controllers/auth/login.js
@@ -0,0 +1,3 @@
+module.exports.getLogin =  (req, res, next) => { //http://localhost:5000/auth/login
+    res.send('Welcome to login page');
+};
---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+// di mana untuk handle setiap function dari route tergantung kebutuhna route minta function yang mana
+const { getLogin }  = require('./auth/login');
+
+module.exports = {
+    getLogin
+};
