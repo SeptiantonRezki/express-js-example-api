3ef31a578a4f69a960c28fd6d85e94afcaac0e2b
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-03 06:58:14 +0700

    menambahkan morgan untuk mengurus Logging System

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,3 +1,6 @@
+const logger = require('../../configuration/logger');
+
 module.exports.getLogin =  (req, res, next) => { //http://localhost:5000/auth/login
+    logger.info("hello");
     res.send('Welcome to login page');
 };
