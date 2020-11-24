5052a582039dbdcfa40a7d63c26d6dffc99588ea
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-25 06:15:32 +0700

    berhasil membuat pagination data movie dengan limit 10 per halaman

--- controllers/movieC.js
+++ controllers/movieC.js
@@ -1,5 +1,18 @@
+const {dbCon} = require('../configuration');
+
+
 const getMovies = (req, res, next) => {
-    res.send('wait for me');
+        const pageNum = parseInt(req.params.page);
+
+        if(isNaN(pageNum)){
+            return res.status(400).send('bad request');
+        }
+
+        const movieToSkip = (pageNum-1) * 10;
+        dbCon('movies', async (db) => {
+            const movies = await db.find({}).skip(movieToSkip).limit(10).toArray();
+            res.json(movies);
+        });
 };
 
 module.exports = {
