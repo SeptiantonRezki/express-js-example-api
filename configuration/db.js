195c689f7bff10992edf481ac7f71fd564306725
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-25 05:50:33 +0700

    berhasil membuat koneksi ke database

---
+++ configuration/db.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const _uri = 'mongodb+srv://septiantonRezki:septian16@projekexamplea.jwtpv.mongodb.net/sample_mflix?retryWrites=true&w=majority';
+const dbCon = (coll, cb) => {
+    MongoClient.connect(_uri)
+    .then(async client => {
+        const db = client.db('sample_mflix').collection(coll);
+        await cb(db);
+        client.close();
+    })
+    .catch();
+};
+
+dbCon('movies', async (db) => {
+    const movie = await db.findOne();
+    console.log(movie);
+})
+
+module.exports= dbCon;
+
