09999f11aae72409f59af42c0b0bcfa94e3fb329
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-26 00:51:09 +0700

    menambahkan model user dengan konek database

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const  {dbCon} = require('../configuration');
+
+class User {
+    constructor(userData){
+        this.userData = {...userData};
+    };
+    save(){
+        dbCon('users', (db) => {
+            db.insertOne(this.userData);
+        });
+    }
+};
+
+const user = new User({
+    username: 'anasSaber',
+    email: 'anas@example.com',
+    password: 'anas1234',
+    first_name: 'Anas',
+    last_name: 'Saber'
+});
+user.save();
+// node .\models\User.js 
