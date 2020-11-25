b56747f73df97f87f09134bcfd510ef54cb5ff24
SeptiantonRezki <septianrezki398@gmail.com>
2020-11-26 02:38:14 +0700

    berhasil menambahkan fitur check adakah user atau email di database checkExistence

--- models/User.js
+++ models/User.js
@@ -1,22 +1,85 @@
-const  {dbCon} = require('../configuration');
+const { dbCon } = require('../configuration');
+const { userValidator } = require('../validator');
 
 class User {
-    constructor(userData){
-        this.userData = {...userData};
+    constructor(userData) {
+        this.userData = { ...userData };
     };
-    save(){
+    save() {
         dbCon('users', (db) => {
             db.insertOne(this.userData);
         });
+    };
+
+    checkExistence() {
+        return new Promise((resolve, reject) => {
+            dbCon('users', async (db) => {
+                try {
+                    const user = await db.findOne({ '$or': [{ username: this.userData['username'] }, { 'email': this.userData['email'] }] });
+                    if (!user) {
+                        resolve({
+                            check: false
+                        })
+                    } else if (this.userData['username'] === user.username) {
+                        resolve({
+                            check: true,
+                            message: 'this username is already in use'
+                        })
+                    } else if (this.userData['email'] === user.email) {
+                        resolve({
+                            check: true,
+                            message: 'this email is already in use'
+                        });
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+
+            });
+        });
+    }
+
+    static validate(userData) {
+        // const result = userValidator.validate(userData);
+        // return result;
+        // console.log(result);
+        // console.log(result.error.message);
+        return userValidator.validate(userData);
     }
 };
 
+// const user = new User({
+//     username: 'anasSaber',
+//     email: 'anas@example.com',
+//     password: 'anas1234',
+//     first_name: 'Anas',
+//     last_name: 'Saber'
+// });
+// user.save();
+
+// const userData = {
+//     username: 'an',
+//     email: 'anas@example.com',
+//     password: 'Anas-1234',
+//     first_name: 'Anas',
+//     last_name: 'Saber'
+// };
+// const validation = User.validate(userData);
+// User.validate(userData);
+
 const user = new User({
-    username: 'anasSaber',
-    email: 'anas@example.com',
+    username: 'anassssSaber',
+    email: 'anasssSaber@example.com',
     password: 'anas1234',
     first_name: 'Anas',
     last_name: 'Saber'
 });
-user.save();
-// node .\models\User.js 
+user.checkExistence()
+.then(check => {
+    console.log(check);
+})
+.catch(err => console.log(err));
+;
+
+
+// node .\models\User.js
--- validator/userValidator.js
+++ validator/userValidator.js
@@ -5,7 +5,13 @@ const Joi = require('@hapi/joi');
 const schema = Joi.object({
     username: Joi.string().alphanum().required().min(3).max(10),
     email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')).required(),
+    password: Joi.string().pattern(
+        new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')
+    )
+    .message('Your password must at least 8 characters and contain at least one upper case letter, one lower case letter, one number and one special character')
+    .required(),
     first_name: Joi.string().required(),
     last_name: Joi.string().required()
 });
+
+module.exports = schema;
